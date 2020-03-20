30589189dfce268d1a72107953a73a2cdb2bbf11
MaximusPeakimus <b6024755@my.shu.ac.uk>
2020-03-20 21:28:27 +0000

    Initial commit

---
+++ app/src/main/java/com/example/groundchecker/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.groundchecker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //hello world
+
+        //test123
+
+        Log.d("TEST", "Hello world")
+    }
+}
